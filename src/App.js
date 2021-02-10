8cbec259ad60e7efbddd5e52c6a36ba6a531dda1
Adam Knight <madak17@gmail.com>
2021-02-10 12:38:22 -0600

    rename app

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>My Notes App</h1>
+      <h1>Test New Changes App</h1>
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
         placeholder="Note name"
