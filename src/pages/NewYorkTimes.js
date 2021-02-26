9bea3f179c81c7bce0203da4e8206cd457f486ab
Adam Knight <madak17@gmail.com>
2021-02-26 16:31:03 -0600

    opening NYT to non-authenticated users and finishing converting XKCD and NYT to hooks

--- src/pages/NewYorkTimes.js
+++ src/pages/NewYorkTimes.js
@@ -2,59 +2,35 @@ import React, { useState, useEffect } from "react";
 import { API, Auth } from 'aws-amplify';
 import NewYorkTimesArticles from '../components/NewYorkTimesArticles';
 
-class NewYorkTimes extends React.Component {
+function NewYorkTimes() {
+    const [nytData, setNytData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            copyright: "",
-            last_updated: null,
-            num_results: 0,
-            results: [],
-            section: "",
-            isLoading: true
-        };
-    }
+    useEffect(() => {
+        getNewYorkTimesWithFetch();
+    }, []);
 
-    async componentDidMount() {
-        const user = await Auth.currentAuthenticatedUser()
-        const token = user.signInUserSession.idToken.jwtToken
-  
-        const requestInfo = {
-            headers: {
-                Authorization: token
-            }
-        }
-  
-        const data = await API.get('ExternalAPIs', '/GetNewYorkTimes', requestInfo)
+    const getNewYorkTimesWithFetch = async () => {
+        const data = await API.get('ExternalAPIs', '/GetNewYorkTimes', '');
+        setNytData(data);
+        setIsLoading(false);
+    };
 
-        this.setState({
-            copyright: data.copyright,
-            last_updated: data.last_updated,
-            num_results: data.num_results,
-            results: data.results,
-            section: data.section,
-            isLoading: false
-        });
-    }
-
-    render() {
-        return this.state.isLoading ? (
-            <div className="nytArticles">
-                <h1>New York Times</h1>
-                <h2>Loading! Please wait...</h2>
-            </div>
-        ) : (
-            <div className="nytArticles">
-                <h1>New York Times</h1>
-                <h2>Copyright: {this.state.copyright}</h2>
-                <h2>Last updated: {this.state.last_updated}</h2>
-                <h2>Number of results: {this.state.num_results}</h2>
-                <h2>News section: {this.state.section}</h2>
-                <NewYorkTimesArticles articles={this.state.results} />
-            </div>
-        );
-      }
+    return isLoading ? (
+        <div className="nytArticles">
+            <h1>New York Times</h1>
+            <h2>Loading! Please wait...</h2>
+        </div>
+    ) : (
+        <div className="nytArticles">
+            <h1>New York Times</h1>
+            <h2>Copyright: {nytData.copyright}</h2>
+            <h2>Last updated: {nytData.last_updated}</h2>
+            <h2>Number of results: {nytData.num_results}</h2>
+            <h2>News section: {nytData.section}</h2>
+            <NewYorkTimesArticles articles={nytData.results} />
+        </div>
+    );
 }
 
 export default NewYorkTimes;
--- src/pages/XkcdComic.js
+++ src/pages/XkcdComic.js
@@ -1,105 +1,86 @@
-import React, { useState, useEffect } from "react";
-import { API, Auth } from 'aws-amplify';
+import { useState, useEffect } from "react";
+import { API } from 'aws-amplify';
 import '../assets/css/XkcdComic.css';
 
 const xkcdComicUrl = "https://xkcd.com/info.0.json";
 
-class XkcdComic extends React.Component {
+function XkcdComic() {
+    const [xkcdComicData, setXkcdComicData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            xkcdComicData: [],
-            isLoading: true
-        };
-    }
+    useEffect(() => {
+        getXkcdComicWithFetch();
+    }, []);
 
-    async componentDidMount() {
-/*         const user = await Auth.currentAuthenticatedUser()
-        const token = user.signInUserSession.idToken.jwtToken
-        console.log({token})
-  
-        const requestInfo = {
-            headers: {
-                Authorization: token
-            }
-        }
-  
-        const data = await API.get('ExternalAPIs', '/GetXkcdComic', requestInfo) */
-
+    const getXkcdComicWithFetch = async () => {
         const data = await API.get('ExternalAPIs', '/GetXkcdComic', '');
+        setXkcdComicData(data);
+        setIsLoading(false);
+    };
 
-        this.setState({
-            xkcdComicData: data,
-            isLoading: false
-        });
-    }
-
-    render() {
-        return this.state.isLoading ? (
-            <div className="XkcdComic">
-                <h1>XKCD Comic</h1>
-                <h2>Loading! Please wait...</h2>
-            </div>
-        ) : (
-            <div className="XkcdComic">
-                <h1>XKCD Comic</h1>
-                <h2>Latest XKCD comic from {xkcdComicUrl}:</h2>
-                <table id="XkcdTable">
-                    <thead>
-                        <tr>
-                            <th>
-                                Number
-                            </th>
-                            <th>
-                                Day
-                            </th>
-                            <th>
-                                Month
-                            </th>
-                            <th>
-                                Year
-                            </th>
-                            <th>
-                                Title
-                            </th>
-                            <th>
-                                Image
-                            </th>
-                            <th>
-                                Alt Text
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                {this.state.xkcdComicData.num}
-                            </td>
-                            <td>
-                                {this.state.xkcdComicData.day}
-                            </td>
-                            <td>
-                                {this.state.xkcdComicData.month}
-                            </td>
-                            <td>
-                                {this.state.xkcdComicData.year}
-                            </td>
-                            <td>
-                                {this.state.xkcdComicData.title}
-                            </td>
-                            <td>
-                                <img src={this.state.xkcdComicData.img} alt={this.state.xkcdComicData.alt} />
-                            </td>
-                            <td>
-                                {this.state.xkcdComicData.alt}
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        );
-      }
+    return isLoading ? (
+        <div>
+            <h1>XKCD Comic</h1>
+            <h2>Loading! Please wait...</h2>
+        </div>
+    ) : (
+        <div>
+            <h1>XKCD Comic</h1>
+            <h2>Latest XKCD comic from {xkcdComicUrl}:</h2>
+            <table id="XkcdTable">
+                <thead>
+                    <tr>
+                        <th>
+                            Number
+                        </th>
+                        <th>
+                            Day
+                        </th>
+                        <th>
+                            Month
+                        </th>
+                        <th>
+                            Year
+                        </th>
+                        <th>
+                            Title
+                        </th>
+                        <th>
+                            Image
+                        </th>
+                        <th>
+                            Alt Text
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            {xkcdComicData.num}
+                        </td>
+                        <td>
+                            {xkcdComicData.day}
+                        </td>
+                        <td>
+                            {xkcdComicData.month}
+                        </td>
+                        <td>
+                            {xkcdComicData.year}
+                        </td>
+                        <td>
+                            {xkcdComicData.title}
+                        </td>
+                        <td>
+                            <img src={xkcdComicData.img} alt={xkcdComicData.alt} />
+                        </td>
+                        <td>
+                            {xkcdComicData.alt}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default XkcdComic;
