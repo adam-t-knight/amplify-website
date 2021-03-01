6d026487d9a94f93296a184e1504b5e0a2c260c9
Adam Knight <madak17@gmail.com>
2021-02-28 19:06:35 -0600

    fixed bug with stock selector

--- src/pages/StockTicker.js
+++ src/pages/StockTicker.js
@@ -3,7 +3,6 @@ import { API } from 'aws-amplify';
 import Stock from '../components/Stock';
 import Emoji from '../components/Emoji';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
-import moment from "moment-timezone";
 import '../assets/css/StockTicker.css';
 
 function StockTicker() {
@@ -12,58 +11,40 @@ function StockTicker() {
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        getStockWithFetch();
-    }, []);
+        getStockWithAPI(stockSymbol);
+    }, [stockSymbol]);
 
-    const getStockWithFetch = async () => {
+    const getStockWithAPI = async () => {
         console.log("Getting stock: " + stockSymbol);
         setIsLoading(true);
         const stockData = await API.get('ExternalAPIs', '/GetStock?symbol=' + stockSymbol, '');
-        console.log(stockData);
         setStockData(stockData);
         setIsLoading(false);
     };
 
-    const handleSelect = (e) => {
-      console.log("Attempting to set stock symbol: " + e);
-      setStockSymbol(e);
-      console.log("Stock symbol: " + stockSymbol)
-      getStockWithFetch();
-    }
-
-    return isLoading ? (
+    return (
         <div className="StockTicker">
             <h1><Emoji symbol="💎🙌" label="diamond hands left"/>Stonk Ticker<Emoji symbol="🙌💎" label="diamond hands right"/></h1>
             <div className="StockContainer">
-                <h2>Loading! Please wait...</h2>
+                
                 <DropdownButton
                     alignRight
-                    title="Dropdown right"
+                    title="Stock selector"
                     id="dropdown-menu-align-right"
-                    onSelect={handleSelect}
+                    onSelect={e => setStockSymbol(e)}
                 >
                     <Dropdown.Item eventKey="GME">GME (XNYS)</Dropdown.Item>
                     <Dropdown.Item eventKey="AMC">AMC (XNYS)</Dropdown.Item>
+                    <Dropdown.Item eventKey="NOK">NOK (XNYS)</Dropdown.Item>
                 </DropdownButton>
-                <h4>You selected {stockSymbol}</h4>                
-                <Stock stockData={stockData} />
-            </div>
-        </div>
-    ) : (
-        <div className="StockTicker">
-            <h1><Emoji symbol="💎🙌" label="diamond hands left"/>Stonk Ticker<Emoji symbol="🙌💎" label="diamond hands right"/></h1>
-            <div className="StockContainer">
-                <DropdownButton
-                    alignRight
-                    title="Dropdown right"
-                    id="dropdown-menu-align-right"
-                    onSelect={handleSelect}
-                >
-                    <Dropdown.Item eventKey="GME">GME (XNYS)</Dropdown.Item>
-                    <Dropdown.Item eventKey="AMC">AMC (XNYS)</Dropdown.Item>
-                </DropdownButton>
-                <h4>You selected {stockSymbol}</h4>                
-                <Stock stockData={stockData} />
+                {isLoading ? (
+                    <div id="StockLoadingMessage">
+                        <h2>Loading! Please wait...</h2>
+                    </div>
+                ) : (
+                    <Stock stockData={stockData} />
+                )}
+                
             </div>
         </div>
     );
