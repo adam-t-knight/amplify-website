d428bd1f6023098d9177564d2554d0186c98bf4f
Adam Knight <madak17@gmail.com>
2021-03-03 10:31:26 -0600

    working update api call

--- src/components/UpdateExercise.js
+++ src/components/UpdateExercise.js
@@ -12,58 +12,32 @@ const UpdateExercise = () => {
     const blankExercise = { name: '', weight: ''}
     const [exerciseState, setExerciseState] = useState([{ ...blankExercise }]);
     
-  const [formData, setFormData] = useState([]);
-  const [authState, setAuthState] = useState();
-  const [user, setUser] = useState();
+    const [authState, setAuthState] = useState();
+    const [user, setUser] = useState();
 
-  useEffect(() => {
-      onAuthUIStateChange((nextAuthState, authData) => {
-          setAuthState(nextAuthState);
-          setUser(authData);
-      });
-      fetchExercises();
-  }, []);
+    useEffect(() => {
+        onAuthUIStateChange((nextAuthState, authData) => {
+            setAuthState(nextAuthState);
+            setUser(authData);
+        });
+        fetchExercises();
+    }, []);
 
-  async function fetchExercises() {
-    const apiData = await API.graphql({ query: listExercises });
-    setExerciseState(apiData.data.listExercises.items);
-  }
-
-/*   async function updateExercise() {
-      console.log("inside update exercise");
-    if (!formData.name || !formData.weight) return;
-        console.log("attempting to change exercise");
-    await API.graphql({ query: updateExerciseMutation, variables: { input: formData } });
-    setExercises([ ...exercises, formData ]);
-    setFormData(initialFormState);
-  } */
-
-  async function testChanges() {
-
-    for (let item of exerciseState) {
-        console.log("exercise name: " + item.name);
-        console.log("exercise weight: " + item.weight);
+    async function fetchExercises() {
+        const apiData = await API.graphql({ query: listExercises });
+        setExerciseState(apiData.data.listExercises.items);
     }
 
-  }
-
-    async function updateExercise({ id , name, weight, createdOn, updatedOn }) {
+    async function updateExercise(idx, { id, name, weight, updatedOn, createdOn }) {
         console.log("inside update exercise");
+        console.log("idx: " + idx);
         console.log("id: " + id);
         console.log("name: " + name);
         console.log("weight: " + weight);
         console.log("createdOn: " + createdOn);
         console.log("updatedOn: " + updatedOn);
-        if (!name || !weight) return;
-        console.log("attempting to change exercise");
-        let newArr = [...exerciseState];
-        console.log("newArr: " + newArr);
-        let newMap = newArr.map();
-        console.log("newMap: " + newMap);
-        await API.graphql({ query: updateExerciseMutation, variables: { input: [...exerciseState].map() } });
-/*         await API.graphql({ query: updateExerciseMutation, variables: { input: id } });
-        setExercises([ ...exercises, formData ]);
-        setFormData(initialFormState); */
+        
+        await API.graphql({ query: updateExerciseMutation, variables: { input: exerciseState[idx] } });
     }
 
     const updateFieldChanged = index => e => {
@@ -80,44 +54,43 @@ const UpdateExercise = () => {
         setExerciseState(newArr);
     }
 
-  return Auth.user ? (
-    <div id="UpdateExercise">
-      <h1>Update Exercise</h1>
-      <Link to="/fitness-tracker">
-        Back
-      </Link>
-      <div style={{marginBottom: 30}}>
-      {
-        exerciseState.map((exercise, idx) => (
-          <div key={exercise.id}>
-            <label>Exercise Name: </label>
-            <input
-                type="text"
-                name="name"
-                value={exercise.name}
-                onChange={updateFieldChanged(idx)}
-            />
-            <label>Exercise Weight: </label>
-            <input
-                type="text"
-                name="weight"
-                value={exercise.weight}
-                onChange={updateFieldChanged(idx)}
-            />
-            <p>Created on: {moment(exercise.createdOn).format('ddd, MMM Do YYYY').toString()}</p>
-            <p>Last update on: {moment(exercise.updatedOn).format('ddd, MMM Do YYYY').toString()}</p>
-            <button onClick={() => updateExercise(exercise)}>Update exercise</button>
-          </div>
-        ))
-      }
-      <button onClick={() => testChanges()}>Test changes</button>
-      </div>
-    </div>
-  ) : (
-    <AmplifyAuthenticator hideDefault={true}>
-      <AmplifySignIn slot="sign-in" hideSignUp />
-    </AmplifyAuthenticator>
-  );
+    return Auth.user ? (
+        <div id="UpdateExercise">
+            <h1>Update Exercise</h1>
+            <Link to="/fitness-tracker">
+                Back
+            </Link>
+            <div style={{marginBottom: 30}}>
+            {
+                exerciseState.map((exercise, idx) => (
+                <div key={exercise.id}>
+                    <label>Exercise Name: </label>
+                    <input
+                        type="text"
+                        name="name"
+                        value={exercise.name}
+                        onChange={updateFieldChanged(idx)}
+                    />
+                    <label>Exercise Weight: </label>
+                    <input
+                        type="text"
+                        name="weight"
+                        value={exercise.weight}
+                        onChange={updateFieldChanged(idx)}
+                    />
+                    <p>Created on: {moment(exercise.createdOn).format('ddd, MMM Do YYYY').toString()}</p>
+                    <p>Last update on: {moment(exercise.updatedOn).format('ddd, MMM Do YYYY').toString()}</p>
+                    <button onClick={() => updateExercise(idx, exercise)}>Update exercise</button>
+                </div>
+                ))
+            }
+            </div>
+        </div>
+    ) : (
+        <AmplifyAuthenticator hideDefault={true}>
+            <AmplifySignIn slot="sign-in" hideSignUp />
+        </AmplifyAuthenticator>
+    );
 }
 
 export default UpdateExercise;
