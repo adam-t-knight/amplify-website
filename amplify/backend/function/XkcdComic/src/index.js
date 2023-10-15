b192a28d05b7258dbf7f42cc0244a2594a0df135
adam-t-knight <78620471+adam-t-knight@users.noreply.github.com>
2023-10-15 20:09:00 +0200

    Updating lambda to log to error

--- amplify/backend/function/XkcdComic/src/index.js
+++ amplify/backend/function/XkcdComic/src/index.js
@@ -2,41 +2,42 @@
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
+  console.info(`EVENT: ${JSON.stringify(event)}`);
 
   const xkcdComicUrl = 'https://xkcd.com/info.0.json';
-
-  fetch(xkcdComicUrl, {
+  
+  await fetch(xkcdComicUrl, {
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers': '*',
     },
   })
-    .then((response) => response.json())
-    // use response of network on fetch Promise resolve
-    .then((data) => {
-      console.log(data);
-      return {
-        statusCode: 200,
-        //  Uncomment below to enable CORS requests
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': '*',
-        },
-        body: JSON.stringify(data),
-      };
-    })
-    // handle fetch Promise error
-    .catch((error) => {
-      console.log(error);
-
-      return {
-        statusCode: 400,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': '*',
-        },
-        body: JSON.stringify(error),
-      };
-    });
+  .then(async (response) => {
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw Error(`${response.status} - ${response.statusText}`);
+    }
+  })
+  .then((responseJson) => {
+    return {
+      "statusCode": 200,
+      "body": JSON.stringify(responseJson),
+      "headers": {
+        "Access-Control-Allow-Origin": '*',
+        "Access-Control-Allow-Headers": '*',
+      },
+    };
+  })
+  .catch((error) => {
+    console.error(error);
+    return {
+      "statusCode": 400,
+      "body": JSON.stringify(error),
+      "headers": {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+      },
+    };
+  });
 };
